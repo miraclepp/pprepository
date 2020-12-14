b9c565b5536c36ee5061902860e54df443e3cf3f
pengpeng <705281580@qq.com>
2020-12-14 09:40:22 +0800

    测试

--- src/com/briup/Test.java
+++ src/com/briup/Test.java
@@ -1,11 +1,31 @@
 package com.briup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test {
+    private String id;
+
+    public Test(String id) {
+        this.id = id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
 
     public static void main(String[] args) {
-        System.out.println("你好");
 
+        double random = Math.random();
+        System.out.println("random");
+        List<Object> list = new ArrayList<>();
 
     }
-    
+
+
+
 }
